ea566ecc35210afb951b64889c4efc38291a10f8
VikramNethesh <vikramnethesh@gmail.com>
2025-10-05 14:21:26 +0530

    upload card complete

--- portal/src/Card.jsx
+++ portal/src/Card.jsx
@@ -1,10 +1,41 @@
-function Card(){
-    return(
-        <div className = "card" >
-            <button className = "card-upload-button">This works</button><br />
-            <button className = "card-submit-button">This too</button>
-        </div>
-    )
+import { useState } from "react";
+import clickMeImg from "./assets/click-me.jpg";
+
+function Card() {
+  const [imageSrc, setImageSrc] = useState(clickMeImg);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImageSrc(reader.result); // update state with uploaded image
+    };
+    reader.readAsDataURL(file);
+  };
+
+  return (
+    <div className="card">
+      <h1 className="Heading">Vehicle Feature Extractor</h1>
+      <label
+        htmlFor="imageUpload"
+        className="card-upload-button"
+        style={{ backgroundImage: `url(${imageSrc})` }}
+      ></label>
+
+      <input
+        type="file"
+        id="imageUpload"
+        accept=".png, .jpg, .jpeg"
+        onChange={handleFileChange}
+        style={{ display: "none" }}
+      />
+
+      <br />
+      <button className="card-submit-button">Analyze</button>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
