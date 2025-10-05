d68cf97cb6e9f3c18da06a640a73183070585ab2
VikramNethesh <vikramnethesh@gmail.com>
2025-10-05 11:19:03 +0530

    starting

---
+++ portal/src/Card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+    return(
+        <div className = "card" >
+            <button className = "card-upload-button">This works</button><br />
+            <button className = "card-submit-button">This too</button>
+        </div>
+    )
+}
+
+export default Card
